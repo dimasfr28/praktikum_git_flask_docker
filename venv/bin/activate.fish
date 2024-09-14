e1d24fbf413ac2d1e286cd189316c4279613fda4
aureliahpsr <aureliahapsari05@gmail.com>
2024-09-14 23:25:23 +0700

    merge by aurel

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dimas/flask_board_p3/venv"
+set -gx VIRTUAL_ENV "/home/aurel/praktikum_git_flask_docker/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
